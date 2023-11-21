400b1d323c636b70b80cf3cf02e3972c929d5683
Alejandro de Brito Fontes <aledbf@gmail.com>
2023-11-21 21:29:14 +0000

    Enable debug

--- src/gitpod-ssh-public-key-auth/install.sh
+++ src/gitpod-ssh-public-key-auth/install.sh
@@ -11,5 +11,6 @@ mkdir -p /etc/gitpod
 chmod 664 /etc/gitpod
 
 sed -i -E "s|#?\s*AuthorizedKeysFile\s+.+|AuthorizedKeysFile /etc/gitpod/public-keys|g" /etc/ssh/sshd_config
+sed -i -E "s|#?\s*LogLevel\s+.+|LogLevel DEBUG3|g" /etc/ssh/sshd_config
 
 echo 'done'
