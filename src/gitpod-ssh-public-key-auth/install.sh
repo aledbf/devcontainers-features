c6e4dd4e52a7b539e333362a5276c9a7ee173fe6
Alejandro de Brito Fontes <aledbf@gmail.com>
2023-11-21 20:01:00 +0000

    Check if sshd_config file exists

--- src/gitpod-ssh-public-key-auth/install.sh
+++ src/gitpod-ssh-public-key-auth/install.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if [ ! -f "/etc/ssh/sshd_config" ]; then
+	echo "Please check the feature ghcr.io/devcontainers/features/sshd is installed."
+	exit 1
+fi
+
 mkdir -p /etc/gitpod
 chmod 664 /etc/gitpod
 
